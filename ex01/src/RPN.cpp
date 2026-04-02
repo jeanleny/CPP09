490452dfd0a6d68ac01f8c7f9da0d09411caa5d5
Leny Peris <lenyperis@hotmail.fr>
2026-04-02 16:31:44 +0200

    oskour

--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -20,15 +20,32 @@ bool validTokens(std::string str)
 	return (true);
 }
 
+bool finalValidation(std::string input)
+{
+	for(size_t i = 0; i < input.size(); i++)
+	{
+		if (!isspace(input[i]))
+			return (false);
+	}
+	return (true);
+}
+
 void ftRpn(char *input)
 {
 	std::stack<int> iStack;
+	int				index;
+	
 	for (size_t i = 0; i < strlen(input); i++)
 	{
-		if (isdigit(input[i]) && (!input[i + 1] || isspace(input[i + 1])))
+		if (iStack.size() < 2)
 		{
-			int	value = input[i] - 48;
-			iStack.push(value);
+			if (isdigit(input[i]) && (!input[i + 1] || isspace(input[i + 1])))
+			{
+				index = i + 2;
+				int	value = input[i] - 48;
+				iStack.push(value);
+				input[i] = ' ';
+			}
 		}
 		else if (isToken(input[i]) &&(!input[i + 1] || isspace(input[i + 1])))
 		{
@@ -50,14 +67,20 @@ void ftRpn(char *input)
 				}
 				if (input[i] == '*')
 					iStack.push(left * right);
+				input[i] = ' ';
+				i = index - 1;
 			}
 			else
 				throw ErrorException();
 		}
-		else if(isspace(input[i]))
+		else if (isspace(input[i]) || isdigit(input[i]))
 			continue ;
 		else
 			throw ErrorException();
 	}
+	if (iStack.size() > 1)
+		throw ErrorException();
+	if (!finalValidation(input))
+		throw ErrorException();
 	std::cout << iStack.top() << std::endl;
 }
